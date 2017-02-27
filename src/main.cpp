781723d54395719ea41c95c3eccfa47d8771df36
Student <student@ib.polsl.pl>
2017-02-27 13:10:28 +0100

    trzeci poprawiony z wlaczona opcja

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 
+#include "LaborkaConfig.h"
+#ifdef USE_TRIGONOMETRY_DEGREE
 #include "trygonometria.h"
+#else
 #include <math.h>
-
+#endif
 
 int main() 
-{
-double result=0;
-result = cos_degree(45.0); 
-
+{double result=0;
+#ifdef USE_TRIGONOMETRY_DEGREE
+result = cos_degree(45.0);
+#else
+result = cos(pi/4.0);
+#endif
 }
