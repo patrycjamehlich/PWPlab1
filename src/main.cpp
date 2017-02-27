064b1c5a739a4f7cc03ef9647334174d04b128df
Student <student@ib.polsl.pl>
2017-02-27 11:43:09 +0100

    trzeci commit z wlaczona opcja

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,12 @@
+#include "laborkaConfig.h"
+#include "trygonometria.h"
+#ifdef USE_TRIGONOMETRY_DEGREE
+result = cos_degree(45.0);
+#else
+result = cos(pi/4.0);
+#endif
+
+
 int main() 
 {
 return 0; 
