4f24dc44d4e2faad493da553db21e8c85a839d5a
Student <student@ib.polsl.pl>
2017-02-27 12:21:21 +0100

    drugi poprawiony

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
+#ifdef USE_TRIGONOMETRY_DEGREE
+#include "trygonometria.h"
+#else
+#include "math.h"
+#endif
+
 int main() 
 {
-return 0; 
+const double pi=3.14;
+double result=0;
+#ifdef USE_TRIGONOMETRY_DEGREE
+result = cos_degree(45.0);
+#else
+result = cos(pi/4.0);
+#endif
+ 
 }
