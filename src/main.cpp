62384a89b7377fa22e1088297bb3358fe1e470a5
Student <student@ib.polsl.pl>
2017-02-27 12:56:02 +0100

    drugi poprawiony2

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,11 @@
-#ifdef USE_TRIGONOMETRY_DEGREE
+
 #include "trygonometria.h"
-#else
-#include "math.h"
-#endif
+#include <math.h>
+
 
 int main() 
 {
-const double pi=3.14;
 double result=0;
-#ifdef USE_TRIGONOMETRY_DEGREE
-result = cos_degree(45.0);
-#else
-result = cos(pi/4.0);
-#endif
- 
+result = cos_degree(45.0); 
+
 }
--- src/trygonometria/trygonometria.cpp
+++ src/trygonometria/trygonometria.cpp
@@ -1,7 +1,12 @@
 #include "trygonometria.h"
 #include <math.h>
 double sin_degree(double degree) {
-//zamiana stopni na radiany: double radian=degree*pi/180.0;
-//return sin(radian); //funkcja biblioteczna z math.h
+const double pi=3.14;
+ double radian=degree*pi/180.0;
+return sin(radian); 
 }
-double cos_degree(double degree) { /*...*/ }
+double cos_degree(double degree) { 
+const double pi=3.14;
+double radian=degree*pi/180.0; 
+return cos(radian);
+}
