f347479b049e9b2258c46505e053c4859aefdc5b
Student <student@ib.polsl.pl>
2017-02-27 13:23:12 +0100

    piaty poprawiony

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: C:/MehlichPwPlab1/PWPlab1/src
+# Build directory: C:/MehlichPwPlab1/PWPlab1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(LaborkaPodstawowyTest "laborka")
+add_test(LaborkaWynikSin45 "Laborka" "45")
+set_tests_properties(LaborkaWynikSin45 PROPERTIES  PASS_REGULAR_EXPRESSION "0[.,]7071")
+add_test(Laborka3sek "laborka")
+set_tests_properties(Laborka3sek PROPERTIES  TIMEOUT "3")
+subdirs(trygonometria)
